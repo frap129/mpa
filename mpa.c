9c52d19c1c76ea0e09129db2e9b8cc1343eb5632
Joe Maples <frap129@gmail.com>
2015-10-24 16:29:50 -0400

    Exit on error

--- mpa.c
+++ mpa.c
@@ -9,10 +9,14 @@
  /* Argument Handling */ 
    if( argc > 3 ) {
       printf("Too many arguments.\n ");
+      exit(1);
    } else if( argc < 2) {
       printf("No directory defined.\n Usage: mpa /patch/directory\n");
+      exit(1);
    } else {
       printf("Using directory %s\n", argv[1]);
+   }
+    
 
    /* Patch Applier */
    system("find argv[1] -name '.patch' -print0 | xargs -0 patch -p1 <");
